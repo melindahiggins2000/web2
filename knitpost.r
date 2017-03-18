849c7ca30493c805978600f936dc61113c5903f0
melindahiggins2000 <melinda.higgins@emory.edu>
2017-03-18 09:35:39 -0400

    test knitpost add new blog post from rmd to _posts

---
+++ knitpost.r
@@ -0,0 +1,30 @@
+# knitpost
+
+library(knitr)
+
+KnitPost <- function(input, base.url = "/") {
+  opts_knit$set(base.url = base.url)
+  fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
+  opts_chunk$set(fig.path = fig.path)
+  opts_chunk$set(fig.cap = "center")
+  render_jekyll()
+  knit(input, envir = parent.frame())
+}
+
+# have the following folders created
+#   _posts
+#   figs
+#   rmd with the rmd files for the posts
+
+# put rmd files in rmd directory
+# run this from root web directory
+
+file <- '2017-03-18-newpost.Rmd'
+runfile <- paste0('cd rmd && copy ',file,' .. && cd ..')
+shell(runfile)
+KnitPost(file)
+shell('move *.md _posts')
+shell(paste0('del ',file))
+
+
+
