ec24f807532fa055bffc5cc8848eaf04b3245510
melindahiggins2000 <melinda.higgins@emory.edu>
2017-03-18 10:14:14 -0400

    comments added to knitpost

--- knitpost.r
+++ knitpost.r
@@ -1,7 +1,11 @@
 # knitpost
+# see initial idea from
+# https://www.r-bloggers.com/blog-with-knitr-and-jekyll/
 
 library(knitr)
 
+# base.url here must include liquid tags
+# for figures to be linked correctly
 KnitPost <- function(input, base.url = "{{ site.url }}{{ site.baseurl }}/") {
   opts_knit$set(base.url = base.url)
   fig.path <- paste0("figs/", 
@@ -20,10 +24,14 @@ KnitPost <- function(input, base.url = "{{ site.url }}{{ site.baseurl }}/") {
 # put rmd files in rmd directory
 # run this from root web directory
 
+# in future - turn this into a function to process
 file <- '2017-03-18-newpost.Rmd'
 runfile <- paste0('cd rmd && copy ',file,' .. && cd ..')
 shell(runfile)
 KnitPost(file)
+
+# this sequence updated to make sure
+# that main README.md is not moved also
 movefile <- paste0("move ",sub(".Rmd$", "", basename(file)),".md _posts")
 shell(movefile)
 shell(paste0('del ',file))
